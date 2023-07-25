b156052c9269b455e0335023cf90779d313544bf
wumengyan <mengyanw@umich.edu>
2023-07-25 16:32:37 -0400

    chore

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,12 @@ import {
   LabShell
 } from '@jupyterlab/application';
 
-import { requestAPI } from './handler';
+// import { requestAPI } from './handler';
 
 import { ITelemetryRouter } from 'telemetry-router';
 
 import { Notebook, NotebookPanel } from '@jupyterlab/notebook';
 
-/**
- * Initialization data for the telemetry-producer extension.
- */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'telemetry-producer:plugin',
   description: 'A JupyterLab extension.',
@@ -25,16 +22,6 @@ const plugin: JupyterFrontEndPlugin<void> = {
     telemetryRouter: ITelemetryRouter
   ) => {
     console.log('JupyterLab extension telemetry-producer is activated!');
-
-    requestAPI<any>('get-example')
-      .then(data => {
-        console.log(data);
-      })
-      .catch(reason => {
-        console.error(
-          `The telemetry_producer server extension appears to be missing.\n${reason}`
-        );
-      });
 
     const labShell = app.shell as LabShell;
     labShell.currentChanged.connect(() => {
