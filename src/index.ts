1f19fa3d21b41519f5cdfb63396fc29e1b8b1317
Mengyan Wu <mengyanw@umich.edu>
2023-08-18 11:09:30 -0400

    feat: new event library

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,20 @@
 import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
-} from '@jupyterlab/application';
+} from '@jupyterlab/application'
 
 import {
   INotebookTracker,
   NotebookPanel
-} from '@jupyterlab/notebook';
+} from '@jupyterlab/notebook'
 
-import { ITelemetryRouter } from 'telemetry-router';
+import { ITelemetryRouter } from 'telemetry-router'
 
-import { requestAPI } from './handler';
+import { requestAPI } from './handler'
 
-import { ETCJupyterLabTelemetryLibrary } from './library';
+import { producerCollection } from './events'
 
-import { INotebookEventMessage } from './types';
-
-// import {
-//   producerCollection,
-// } from './events';
-
-const PLUGIN_ID = 'telemetry-producer:plugin';
+const PLUGIN_ID = 'telemetry-producer:plugin'
 
 const plugin: JupyterFrontEndPlugin<void> = {
   id: PLUGIN_ID,
@@ -39,50 +33,21 @@ const plugin: JupyterFrontEndPlugin<void> = {
     console.log(`${PLUGIN_ID}: ${version}`)
 
     const config = await requestAPI<any>('config')
-    notebookTracker.widgetAdded.connect(async (notebookTracker: INotebookTracker, notebookPanel: NotebookPanel) => {
-      await notebookPanel.sessionContext.ready // wait until session id is created
-      const eventLibrary = new ETCJupyterLabTelemetryLibrary({ notebookPanel, config })
-      const signals = [
-        eventLibrary.activeCellChangeEvent.activeCellChanged,
-        eventLibrary.cellAddEvent.cellAdded,
-        eventLibrary.cellErrorEvent.cellErrored,
-        eventLibrary.cellExecutionEvent.cellExecuted,
-        eventLibrary.cellRemoveEvent.cellRemoved,
-        eventLibrary.notebookClipboardEvent.notebookClipboardCopied,
-        eventLibrary.notebookClipboardEvent.notebookClipboardCut,
-        eventLibrary.notebookClipboardEvent.notebookClipboardPasted,
-        eventLibrary.notebookCloseEvent.notebookClosed,
-        eventLibrary.notebookOpenEvent.notebookOpened,
-        eventLibrary.notebookSaveEvent.notebookSaved,
-        eventLibrary.notebookVisibilityEvent.notebookHidden,
-        eventLibrary.notebookVisibilityEvent.notebookVisible,
-        eventLibrary.notebookScrollEvent.notebookScrolled
-      ];
 
-      await telemetryRouter.loadNotebookPanel(notebookPanel)
-      signals.forEach((signal) => signal.connect(
-        async (_, message: INotebookEventMessage) => {
-          const eventDetail = {
-            eventName: message.eventName,
-            eventTime: Date.now(),
-            cells: message.cells,
-            kernelError: message.kernelError, //  For cellErrored event.
-            selection: message.selection, //  For notebookClipboard event.
-            environ: message.environ,  //  For openNotebook event.
+    notebookTracker.widgetAdded.connect(
+      async (_, notebookPanel: NotebookPanel) => {
+        await notebookPanel.sessionContext.ready // wait until session id is created
+        await telemetryRouter.loadNotebookPanel(notebookPanel)
+
+        producerCollection.forEach((producer) => {
+          if (config.activeEvents.includes(producer.id)) {
+            console.log(producer.id)
+            new producer().listen(notebookPanel, telemetryRouter, config.logNotebookContentEvents.includes(producer.id))
           }
-          const logNotebookContentOrNot: boolean = config.logNotebookContentEvents.includes(message.eventType)
-          await telemetryRouter.publishEvent(eventDetail, logNotebookContentOrNot)
-        }
-      ))
-
-      // producerCollection.forEach((producer) => {
-      //   if (config.includes(producer.id)) {
-      //     new producer().listen(notebookTracker, telemetryRouter)
-      //   }
-      // })
-    }
+        })
+      }
     )
   }
-};
+}
 
-export default plugin;
+export default plugin
